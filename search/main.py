3ee4ae209f4465546d3dd7fefcecaf2d359f8c60
junlic <junlic@student.unimelb.edu.au>
2021-03-10 11:58:00 +0800

    added board.py for board base code

---
+++ search/main.py
@@ -0,0 +1,35 @@
+"""
+COMP30024 Artificial Intelligence, Semester 1, 2021
+Project Part A: Searching
+
+This script contains the entry point to the program (the code in
+`__main__.py` calls `main()`). Your solution starts here!
+"""
+
+import sys
+import json
+
+# If you want to separate your code into separate files, put them
+# inside the `search` directory (like this one and `util.py`) and
+# then import from them like this:
+from search.util import print_board, print_slide, print_swing
+from search.board import create_token_dict, generate_board
+
+def main():
+    try:
+        with open(sys.argv[1]) as file:
+            data = json.load(file)
+            token_dict = create_token_dict(data)
+            board_dict = generate_board(token_dict)
+            print_board(board_dict, compact=True)
+
+    except IndexError:
+        print("usage: python3 -m search path/to/input.json", file=sys.stderr)
+        sys.exit(1)
+
+    # TODO:
+    # Find and print a solution to the board configuration described
+    # by `data`.
+    # Why not start by trying to print this configuration out using the
+    # `print_board` helper function? (See the `util.py` source code for
+    # usage information).
