ef40ee479ca5a7f2e20af9b8b14958b7e3c0bdc8
Cedric0303 <chenjunli7@gmail.com>
2021-03-12 23:52:22 +0800

    created board class and base code for token class

--- search/main.py
+++ search/main.py
@@ -13,15 +13,14 @@ import json
 # inside the `search` directory (like this one and `util.py`) and
 # then import from them like this:
 from search.util import print_board, print_slide, print_swing
-from search.board import create_token_dict, generate_board
+import search.board
 
 def main():
     try:
         with open(sys.argv[1]) as file:
             data = json.load(file)
-            token_dict = create_token_dict(data)
-            board_dict = generate_board(token_dict)
-            print_board(board_dict, compact=True)
+            board = search.board.Board(data)
+            print_board(board.get_board_dict(), compact=True)
 
     except IndexError:
         print("usage: python3 -m search path/to/input.json", file=sys.stderr)
---
+++ token.py
@@ -0,0 +1,8 @@
+class Token():
+    def __init__(self) -> None:
+        pass
+
+#class Upper(Token):
+
+
+#class Block(Token):
